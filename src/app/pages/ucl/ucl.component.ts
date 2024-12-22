5af2c2a3c88252a392d2835f20b05718fbdc7607
AlaBhs <alabelhadjsalem@gmail.com>
2024-12-22 22:30:56 +0100

    styling the ucl component

--- src/app/pages/ucl/ucl.component.ts
+++ src/app/pages/ucl/ucl.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { FootballDataService } from '../../services/football-data.service';
 
 @Component({
   selector: 'app-ucl',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './ucl.component.html',
   styleUrl: './ucl.component.css'
 })
-export class UclComponent {
+export class UclComponent implements OnInit {
+  groupStageRankings: any[] = [];
 
+  constructor(private footballDataService: FootballDataService) { }
+
+  ngOnInit(): void {
+    this.fetchGroupStageRankings();
+  }
+
+  fetchGroupStageRankings(): void {
+    this.footballDataService.getUCLGroupStageRankings().subscribe({
+      next: (data: any) => {
+        this.groupStageRankings = data.standings || []; 
+        console.log(this.groupStageRankings);
+      },
+      error: (error: any) => {
+        console.error('Error fetching UCL rankings:', error);
+      }
+    });
+  }
 }
