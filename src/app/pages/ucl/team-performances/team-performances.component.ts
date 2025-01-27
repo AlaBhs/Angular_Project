7f434e07ea8a50fba5d1bdc67025a62a9ba84434
AlaBhs <alabelhadjsalem@gmail.com>
2025-01-27 23:30:41 +0100

    start team performances

--- src/app/pages/ucl/team-performances/team-performances.component.ts
+++ src/app/pages/ucl/team-performances/team-performances.component.ts
@@ -1,112 +1,45 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { Club } from '../dto/ucl-club-details.dto';
-import { MatTableDataSource, MatTableModule } from '@angular/material/table';
-import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
-import { MatSort, MatSortModule } from '@angular/material/sort';
-import { MatButtonModule } from '@angular/material/button';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ClubItemComponent } from '../club-item/club-item.component';
 import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { ClubService } from './team-performances.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-team-performances',
   standalone: true,
-  imports: [
-    MatTableModule,
-    MatPaginatorModule,
-    MatSortModule,
-    MatButtonModule,
-    BrowserAnimationsModule,CommonModule,
-  ],
+  imports: [ClubItemComponent, CommonModule, RouterOutlet, FormsModule],
   templateUrl: './team-performances.component.html',
-  styleUrl: './team-performances.component.css',
+  styleUrls: ['./team-performances.component.css'],
 })
-export class TeamPerformancesComponent {
-  displayedColumns: string[] = [
-    'position',
-    'name',
-    'country',
-    'participated',
-    'titles',
-    'played',
-    'win',
-    'draw',
-    'loss',
-    'goalsFor',
-  ];
+export class TeamPerformancesComponent implements OnInit{
+  selectedClub: number | null = null;
+  selectedCountry: string = '';
+  searchByName: string = '';
+  private clubService = inject(ClubService);
 
-  dataSource = new MatTableDataSource<Club>([
-    {
-      position: 1,
-      name: 'Real Madrid CF',
-      country: 'ESP',
-      participated: 53,
-      titles: 14,
-      played: 476,
-      win: 285,
-      draw: 81,
-      loss: 110,
-      goalsFor: 1047,
-    },
-    {
-      position: 2,
-      name: 'FC Bayern München',
-      country: 'GER',
-      participated: 39,
-      titles: 6,
-      played: 382,
-      win: 229,
-      draw: 76,
-      loss: 77,
-      goalsFor: 804,
-    },
-    {
-      position: 3,
-      name: 'FC Barcelona',
-      country: 'ESP',
-      participated: 33,
-      titles: 5,
-      played: 339,
-      win: 197,
-      draw: 76,
-      loss: 66,
-      goalsFor: 667,
-    },
-    {
-      position: 4,
-      name: 'Manchester United',
-      country: 'ENG',
-      participated: 30,
-      titles: 3,
-      played: 293,
-      win: 160,
-      draw: 69,
-      loss: 64,
-      goalsFor: 533,
-    },
-    {
-      position: 5,
-      name: 'Juventus',
-      country: 'ITA',
-      participated: 37,
-      titles: 2,
-      played: 301,
-      win: 153,
-      draw: 70,
-      loss: 78,
-      goalsFor: 479,
-    },
-  ]);
+  clubData: Observable<Club[]> | null = null ;
+  filteredClubs: Observable<Club[]> | null = null ;
 
-  @ViewChild(MatPaginator) paginator!: MatPaginator;
-  @ViewChild(MatSort) sort!: MatSort;
-
-  ngOnInit() {
-    this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
+  ngOnInit(): void {
+    this.clubData = this.clubService.getClubs();
+    this.filteredClubs = this.clubData;
+  }
+  selectClub(club: number): void {
+    this.selectedClub = club;
   }
 
-  applyFilter(event: Event) {
-    const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
+  onSearchChange(): void {
+    this.filteredClubs = this.clubService.onSearchAndFilter(this.searchByName, this.selectedCountry);
+  }
+
+  filterClubs(): void {
+    if (this.selectedCountry || this.searchByName) {
+      this.filteredClubs = this.clubService.onSearchAndFilter(this.searchByName, this.selectedCountry);
+    } else {
+      this.filteredClubs = this.clubData;
+    }
   }
 }
